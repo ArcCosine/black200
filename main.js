c648174fdd38159012c3b343a6728b3a3a5b6be0
ArcCosine <40669+ArcCosine@users.noreply.github.com>
2022-05-13 14:10:27 +0900

    [Update] Update development enviroment.
    Change Styyle White to Black.
    Remove style attribute.
    Update vite code.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import './style.css'
 
 const shuffle = ([...array]) => {
     for (let i = array.length - 1; i > 0; i--) {
@@ -8,14 +9,14 @@ const shuffle = ([...array]) => {
 }
 
 function ColorToHex(color) {
-    var hexadecimal = color.toString(16);
-    return hexadecimal.length == 1 ? "0" + hexadecimal : hexadecimal;
+    const hexadecimal = color.toString(16);
+    return ("00" + hexadecimal).slice(-2);
 }
 function ConvertRGBtoHex(col) {
     return ColorToHex(col[0]) + ColorToHex(col[1]) + ColorToHex(col[2]);
 }
 
-let ac_color = [-1, -1, -1];
+let ac_color = [255, 255, 255];
 let ac_color_hex = "";
 let color_step = 0;
 let start_time;
@@ -24,11 +25,8 @@ let score_num = 0;
 let enable_submit = 0;
 
 function update_score(){
-    const try_elem = document.getElementById('try');
-    try_elem.innerHTML = "Try: " + try_num;
-
-    const score_elem = document.getElementById('score');
-    score_elem.innerHTML = "Score: " + score_num;
+    document.getElementById('try').innerHTML = "Try: " + try_num;
+    document.getElementById('score').innerHTML = "Score: " + score_num;
 }
 
 function submit(r, g, b) {
@@ -38,8 +36,8 @@ function submit(r, g, b) {
     const col_hex = ConvertRGBtoHex(col);
     try_num++;
 
-    if (col_hex == ac_color_hex) {
-        time = update_timer();
+    if (col_hex === ac_color_hex) {
+        let time = update_timer();
         enable_submit = false;
         update_score();
         document.getElementById('modal_ac').style.visibility ="visible";
@@ -52,21 +50,21 @@ function submit(r, g, b) {
 
         let comment = "";
         let comment2 = "";
-        if (color_step != 1) {
+        if (color_step !== 1) {
             comment = "| チュートリアルを完了しました！"
             comment2 = "チュートリアル終了！"
-        } else if(try_num == 1) {
-            comment = "| 一発で白を見つけられました！"
-            comment2 = "一発で白を見つけられた！"
+        } else if(try_num === 1) {
+            comment = "| 一発で黒を見つけられました！"
+            comment2 = "一発で黒を見つけられた！"
         } else if(time <= 10000) {
-            comment = "| 素早くで白を見つけられました！"
-            comment2 = "素早く白を見つけられた！"
+            comment = "| 素早くで黒を見つけられました！"
+            comment2 = "素早く黒を見つけられた！"
         }
 
         document.getElementById('tw_share').setAttribute('href',
             "http://twitter.com/share?url=" + encodeURI(location.href) +
-            "&hashtags=white_200&related=TumoiYorozu" +
-            "&text="+encodeURI("200色の白から見つけよう！ White 200 "+comment+"\n得点:" + score_num + " 時間:" + time_text + "秒 クリック回数:" + try_num)
+            "&hashtags=black_200&related=ArcCosine" +
+            "&text="+encodeURI(`200色の黒から見つけよう！ Black 200 ${comment}\n得点:${score_num} 時間:${time_text}秒 クリック回数:${try_num}`)
         );
         
         document.getElementById('ac_comment').innerHTML = comment2;
@@ -101,44 +99,45 @@ function update_timer(){
 
 function make_problem(num, dif){
     const start_btn = document.getElementById('start_btn');
-    if (start_btn != null) start_btn.remove();
+    if (start_btn !== null) start_btn.remove();
 
     color_step = dif;
     const alt = document.getElementById('list');
-    const target_col = [255, 255, 255];
+    const target_col = [0, 0, 0];
     alt.innerHTML="";
     let list = [];
     for(let r = 0; r <= 5; ++r){
         for(let g = 0; g <= 5; ++g){
             for(let b = 0; b <= 5; ++b){
-                list.push([target_col[0]-r*dif, target_col[1]-g*dif, target_col[2]-b*dif]);
+                list.push([target_col[0]+r*dif, target_col[1]+g*dif, target_col[2]+b*dif]);
             }
         }
     }
     list = shuffle(list);
     let html = "";
+    const fragment = document.createDocumentFragment();
     for(let i = 0; i < num; ++i) {
         let col_hex = ConvertRGBtoHex(list[i]);
-        html += '<div'+
-            ' class="alternatives"'+
-            ' id=col_' + col_hex +
-            ' onclick=submit(' + list[i][0]+","+list[i][1]+","+list[i][2] + ')'+
-            ' style="background-color:#'+ col_hex +
-            '"></div>\n';
-            // '">#' + col_hex + '</div>\n';
+        const div = document.createElement('button');
+        div.classList.add('alternatives');
+        div.id = `col_${col_hex}`;
+        div.addEventListener('click', submit.bind(this,list[i][0], list[i][1], list[i][2]), false );
+        div.style.backgroundColor = `#${col_hex}`; 
+        fragment.appendChild(div);
     }
-    alt.innerHTML=html;
+    alt.innerHTML="";
+    alt.appendChild(fragment);
     ac_color = list[Math.floor(Math.random() * num)];
     ac_color_hex = ConvertRGBtoHex(ac_color);
     const problem_text = document.getElementById('problem_text');
-    problem_text.innerHTML = "この白を探せ！<br>#" + ac_color_hex;
+    problem_text.innerHTML = "この黒を探せ！<br>#" + ac_color_hex;
     const problem_color_box = document.getElementById('problem_color_box');
     problem_color_box.style.setProperty('background-color', "#" + ac_color_hex);
 
     
     document.getElementById('modal_ac').style.visibility ="hidden";
 
-    score_num = (dif == 1) ? 1000 : 100;
+    score_num = (dif === 1) ? 1000 : 100;
     try_num = 0;
     update_score();
 
@@ -147,7 +146,27 @@ function make_problem(num, dif){
     update_timer();
 }
 
-window.onload = function(){
-    // make_problem(200, 2);
-    // make_problem(3, 2);
+function postTwitter(){
+    const twitterUrl = document.getElementById('tw_share').href;
+    window.open(encodeURI(decodeURI(twitterUrl)), 'tweetwindow', 'width=650, height=470, personalbar=0, toolbar=0, scrollbars=1, sizable=1');
 }
+
+
+function init(){
+    // Tutorial
+    document.getElementById('start_btn').addEventListener('click', function(){
+        make_problem(200,8);
+    }, false );
+
+
+    //production
+    document.getElementById('start_pro').addEventListener('click', function(){
+        make_problem(200,1);
+    }, false );
+
+
+    // Share Twitter
+    document.getElementById('tw_share').addEventListener('click', postTwitter, false );
+}
+
+document.addEventListener('DOMContentLoaded', init , false );
